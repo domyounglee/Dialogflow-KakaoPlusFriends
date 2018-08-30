153e303ce790334d4f875f7d8ccf0a821853a1fc
Ubuntu <ubuntu@ip-172-31-16-12.ap-northeast-2.compute.internal>
2018-08-30 15:25:17 +0000

    views update

--- dialogflow/views.py
+++ dialogflow/views.py
@@ -3,9 +3,52 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import requests
 import json
+
+
+ERROR_MESSAGE = '네트워크 접속에 문제가 발생하였습니다. 잠시 후 다시 시도해주세요.'
+URL_OPEN_TIME_OUT = 10
  
  
+#----------------------------------------------------
+# Dialogflow에서 대답 구함
+#----------------------------------------------------
+def get_answer(text, user_key):
+    
+    #--------------------------------
+    # Dialogflow에 요청
+    #--------------------------------
+    data_send = { 
+        'lang': 'ko',
+        'query': text,
+        'sessionId': user_key,
+        'timezone': 'Asia/Seoul'
+    }
+    
+    data_header = {
+        'Content-Type': 'application/json; charset=utf-8',
+        'Authorization': 'Bearer 67de7957314447fd80e3cba8beae6a66' # Dialogflow의 Client access token 입력
+    }
+    
+    dialogflow_url = 'https://api.dialogflow.com/v1/query?v=20150910'
+    
+    res = requests.post(dialogflow_url,
+                            data=json.dumps(data_send),
+                            headers=data_header)
+
+    #--------------------------------
+    # 대답 처리
+    #--------------------------------
+    if res.status_code != requests.codes.ok:
+        return ERROR_MESSAGE
+    
+    data_receive = res.json()
+    answer = data_receive['result']['fulfillment']['speech'] 
+    
+    return answer
+
+
 def keyboard(request):
     return JsonResponse({
        'type' : 'buttons',
@@ -21,6 +64,22 @@ def message(request) :
     user_key = return_json_str['user_key']
     content_type = return_json_str['type']     
     
+    if len(user_key) <= 0 or len(content) <= 0:
+        speech = ERROR_MESSAGE
+    elif content == '대화하기':
+        speech = '안녕하세요! 전 피자 주문을 받는 챗봇입니다~'
+    else:
+        speech = get_answer(content, user_key)
+    
+    return JsonResponse({
+        'message' : {
+        'text' : speech
+        },
+        'keyboard' : {
+        'type' : 'text' # 텍스트로 입력받기 위하여 키보드 타입을 text로 설정
+        }
+    })
+    """
     if return_str == '썸톡번역기' :
         return JsonResponse({
              'message' : {
@@ -40,4 +99,5 @@ def message(request) :
                         'buttons': ['1','2']
                 }
         })
+    """
  
